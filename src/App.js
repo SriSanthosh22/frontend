38bc1aad7564aa925803cbf45ccbde3acd9d3725
sri santhosh <srisanthosh145@gmail.com>
2022-08-12 14:26:55 +0530

    fdasa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/Footer.js';
+import Terms from './components/Terms/Terms';
+import { BrowserRouter ,Route, Router, Routes} from 'react-router-dom';
+import Policy from './components/Policy/Policy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <BrowserRouter>
+      
+      <Routes>
+      
+      <Route path='/Terms' element={<Terms/>} />
+      <Route path='/Policy' element={<Policy/>} />
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    
     </div>
   );
 }
